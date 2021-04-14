61639d797105146d1bc1e6af018e814abc101c1b
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-04-14 17:03:54 +0100

    added reusable header and footer

---
+++ components/footer.php
@@ -0,0 +1,10 @@
+<footer class="c-footer">
+        <div class="l-restrict">
+            &#169; 2021
+        </div>
+    </footer>
+    <script type="module" src="/assets/js/bundle.js?v=<?php echo rand() ?>"></script>
+    <script src="https://kit.fontawesome.com/6b035dc64a.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,51 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/assets/css/base.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/layout.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/stylesheet.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" media="print" onload="this.media='all'" />
-    <title>Three's Company</title>
-</head>
-
-<body>
-    <div class="l-wrap l-wrap--bg-secondary l-sticky-top l-border-bottom-grey">
-        <nav class="l-restrict c-banner">
-            <a href="/" class="c-banner__logo">
-                <img src="/resources/logos/threescompany.png" class="c-banner__logo-img">
-            </a>
-            <div class="c-navigation" data-type="dynamic">
-                <ul class="c-navbar">
-                    <li class="c-navbar__element">
-                        <a href="/" class="c-navbar__link c-navbar__link--hover-invert">Home</a>
-                    </li>
-                    <li class="c-navbar__element">
-                        <a href="/companies" class="c-navbar__link c-navbar__link--hover-invert">Companies</a>
-                    </li>
-                </ul>
-                <div class="c-hamburger">
-                    <button class="c-hamburger__button">
-                        <div class="c-hamburger__line"></div>
-                        <div class="c-hamburger__line"></div>
-                        <div class="c-hamburger__line"></div>
-                    </button>
-                    <ul class="c-hamburger__menu" data-type="slideover" data-expand="accordian">
-                        <li class="c-hamburger__element">
-                            <a href="/" class="c-hamburger__link c-hamburger__link--hover-invert">Home</a>
-                        </li>
-                        <li class="c-hamburger__element">
-                            <a href="/companies" class="c-hamburger__link c-hamburger__link--hover-invert">Companies</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
+<?php include("./components/header.php") ?>
     <header class="c-hero c-hero--light c-hero--small l-wrap">
         <div class="c-hero__title-wrapper c-hero__title-wrapper--no-bg">
             <h1 class="c-hero__title">
@@ -367,13 +320,6 @@
             </div>
         </div>
     </div>
-    <footer class="c-footer">
-        <div class="l-restrict">
-            &#169; 2021
-        </div>
-    </footer>
-    <script type="module" src="assets/js/bundle.js?v=<?php echo rand() ?>"></script>
-    <script src="https://kit.fontawesome.com/6b035dc64a.js" crossorigin="anonymous"></script>
-</body>
 
-</html>
+
+<?php include ("./components/footer.php") ?>
